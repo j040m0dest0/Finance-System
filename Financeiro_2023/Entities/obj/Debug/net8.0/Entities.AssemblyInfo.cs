0ecaec86bab801811aa797a2ccc8e97c3305e15e
j040m0dest0 <jvictormc01@gmail.com>
2024-11-16 18:24:10 -0300

    Criação dos projetos separados por pastas.

--- Financeiro_2023/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Financeiro_2023/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64c82568820ed527f17251267c0b7b21543b07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abfaa60fc30ba020b02996507da34d1ae4893ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
